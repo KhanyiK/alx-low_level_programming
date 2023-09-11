6796cbbe64486ae0614565a9ef1663f57dd97891
KhanyiK <kskosana03@gmail.com>
2023-09-11 14:21:52 -0700

    variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - prints numbers from o to 9
+ *
+ *Return: always 0 (correct)
+ */
+int main(void)
+{
+	int a:
+
+	for (a = 0, a < 10, a++)
+	{
+		printf("%d", a);
+	}
+	putchar(\n);
