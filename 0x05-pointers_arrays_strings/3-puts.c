1a64f6d38c87ba9673dd92c87ecc4dba9a781732
KhanyiK <kskosana03@gmail.com>
2023-09-19 06:38:02 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#iclude "main.h"
+/**
+  * _puts - prints a string
+  * @str: string
+  * Return: void
+  */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
