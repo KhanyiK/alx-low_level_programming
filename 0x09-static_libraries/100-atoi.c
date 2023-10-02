f97671ae5c9e9ab61176072a33d07bb3b0dacf55
KhanyiK <kskosana03@gmail.com>
2023-10-02 14:24:00 -0700

    static libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -10,7 +10,7 @@ int _atoi(char *s)
 	int x = 0;
 	unsigned int ni = 0;
 	int min = 1;
-	int isi = 0;
+	int is = 0;
 
 	while (s[x])
 	{
@@ -20,11 +20,11 @@ int _atoi(char *s)
 	}
 	while (s[x] >= 48 && s[x] <= 57)
 	{
-	isi = 1;
+	is = 1;
 	ni = (ni * 10) + (s[x] - '0');
 	x++;
 	}
-	if (isi == 1)
+	if (is == 1)
 	{
 	break;
 	}
