85f54da8a699f403df673b728ae025fb336ff71c
KhanyiK <kskosana03@gmail.com>
2023-10-10 15:42:47 -0700

    structures typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * struct dog - defines a new type of struct dog
+  * @name: input
+  * @age: input
+  * @owner: input
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
