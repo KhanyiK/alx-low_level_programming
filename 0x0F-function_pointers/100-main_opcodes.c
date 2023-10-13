70c0544a0e2a9a10d7bf5f399dc3f9bcf5d9ee90
KhanyiK <kskosana03@gmail.com>
2023-10-13 16:39:06 -0700

    function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
-  * main - prints opcodes
+  * main - prints opcodes its own opcodes
   * @argc: arguments
   * @argv: array
   * Return: 0
@@ -10,30 +10,30 @@
 int main(int argc, char *argv[])
 {
 	int b, x;
-	char *ar;
+	char *arr;
 
 	if (argc != 2)
 	{
-		printf("Error\n");
+		printf("error\n");
 		exit(1);
 	}
 	b = atoi(argv[1]);
 
 	if (b < 0)
 	{
-		printf("Error\n");
+		printf("error\n");
 		exit(2);
 	}
-	ar = (char *)main;
+	arr = (char *)main;
 
 	for (x = 0; x < b; x++)
 	{
 		if (x == b - 1)
 		{
-			printf("%02hhx\n", ar[x]);
+			printf("%02hhx\n", arr[x]);
 			break;
 		}
-		printf("%02hhx", ar[x]);
+		printf("%02hhx", arr[x]);
 	}
 	return (0);
 }
