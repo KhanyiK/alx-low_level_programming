87931b02a7bb50839d70dce7048e11faa1d8fc68
KhanyiK <141050598+KhanyiK@users.noreply.github.com>
2023-10-31 13:39:17 +0200

    Delete 0x0B-malloc_free directory

--- 0x0B-malloc_free/main.h
+++
@@ -1,9 +0,0 @@
-#ifdef MAIN_H
-#define MAIN_H
-
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-char *argstostr(int ac, char **av);
-
-#endif
