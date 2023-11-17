2346c9833fc6b9e6fedf099abaad06fb1fbef14a
KhanyiK <kskosana03@gmail.com>
2023-11-17 08:08:37 -0800

    singly linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,20 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t x;
-x = 0;
+	int x;
 
-while (h != NULL)
-{
-if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)");
-else
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-x++;
-}
-return (x);
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil\n)");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		x++;
+		h = h->next;
+	}
+	return (x);
 }
