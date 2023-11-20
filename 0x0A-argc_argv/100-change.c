92abf7fef6ce8739d55c1cad081696644731c9e1
KhanyiK <kskosana03@gmail.com>
2023-11-20 15:46:40 -0800

    argc argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 	int x, answer;
 	unsigned int y;
 	char *z;
-	int coins [] = {25, 10, 5, 2};
+	int cents[] = {25, 10, 5, 2};
 
 	if (argc != 2)
 	{
@@ -26,12 +26,12 @@ int main(int argc, char **argv)
 	{
 		while (x > 1)
 	{
-	for (y = 0; y < sizeof(coins[y]); y++)
+	for (y = 0; y < sizeof(cents[y]); y++)
 	{
-		if (x >= coins[y])
+		if (x >= cents[y])
 		{
-			answer += x / coins[y];
-			x = x % coins[y];
+			answer += x / cents[y];
+			x = x % cents[y];
 		}
 	}
 	}
